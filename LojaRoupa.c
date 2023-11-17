602e9a933beb694eded0e78c1dcb6374c66bd49f
Cecilia Cortes Lopes <105022346+CeciliaCortesL@users.noreply.github.com>
2023-11-17 19:30:06 -0300

    Trabalho Final - Loja de Roupas (Código Final)

--- LojaRoupa.c
+++ LojaRoupa.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h> 
 
-/*define duas estruturas: Roupa e Cliente. Cada uma representa um tipo de dado que 
+/*define duas estruturas: Roupa, Cliente e fornecedor. Cada uma representa um tipo de dado que 
 pode ser utilizado para armazenar informações sobre roupas e clientes, respectivamente.*/
 typedef struct {
     char descricao[50];
@@ -22,27 +22,44 @@ typedef struct {
     char uf[3];
 } Cliente;
 
+typedef struct {
+    char nome[50];
+    char enderecoEletronico[50];
+    char email[50];
+    char telefone[15];
+    char endereco[100];
+    char uf[3];
+} Fornecedor;
+
 /*Essa parte do código declara as funções que serão utilizadas posteriormente no programa. 
 Cada função tem uma responsabilidade específica, como listar roupas, listar clientes, 
 listar clientes de um estado específico, ordenar roupas por preço e ordenar clientes por nome.*/
 void listarRoupas(Roupa roupas[], int n);
 void listarClientes(Cliente clientes[], int n);
+void listarFornecedores(Fornecedor fornecedores[], int n);
 void listarClientesEstado(Cliente clientes[], int n, char estado[]);
 void ordenarRoupasPorPreco(Roupa roupas[], int n);
 void ordenarClientesPorNome(Cliente clientes[], int n);
+void ordenarFornecedoresPorNome(Fornecedor fornecedores[], int n);
 
 int main() {
-    // Registrar 3 roupas e 3 clientes
+    // Registrar 3 roupas, 3 clientes e 3 fornecedores
     Roupa roupas[] = {
-        {"Camiseta", "Manga Curta", "M", 29.99, "Nike", "Azul"},
-        {"Calça Jeans", "Jeans", "36", 79.99, "Levi's", "Azul"},
-        {"Vestido", "Curto", "P", 49.99, "Zara", "Vermelho"}
+        {"Camiseta", "Manga Curta", "M", 29.99, "MarcaA", "Azul"},
+        {"Calça Jeans", "Jeans", "36", 79.99, "MarcaB", "Azul"},
+        {"Vestido", "Curto", "P", 49.99, "MarcaC", "Vermelho"}
     };
 
     Cliente clientes[] = {
         {"Cliente1", 25, "cliente1@email.com", "123456789", "Endereco1", "SP"},
         {"Cliente2", 30, "cliente2@email.com", "987654321", "Endereco2", "RJ"},
         {"Cliente3", 22, "cliente3@email.com", "111222333", "Endereco3", "MG"}
+    };
+
+     Fornecedor fornecedores[] = {
+        {"Fornecedor1", "enderecoEletronico1", "fornecedor1@email.com", "111111111", "Endereco4", "SP"},
+        {"Fornecedor2", "enderecoEletronico2", "fornecedor2@email.com", "222222222", "Endereco5", "RJ"},
+        {"Fornecedor3", "enderecoEletronico3", "fornecedor3@email.com", "333333333", "Endereco6", "MG"}
     };
 
     /*Dentro de um loop do-while, o programa exibe um menu para o usuário com várias opções. 
@@ -56,6 +73,8 @@ int main() {
         printf("3. Listar os clientes de um determinado estado\n");
         printf("4. Listar todas as roupas em ordem crescente de preço\n");
         printf("5. Listar todos os clientes em ordem crescente de nome\n");
+        printf("6. Listar todos os fornecedores\n");
+        printf("7. Listar os fornecedores em ordem crescente de nome\n");
         printf("0. Sair\n");
 
         printf("\nEscolha uma opção: ");
@@ -87,6 +106,13 @@ int main() {
             case 5:
                 ordenarClientesPorNome(clientes, sizeof(clientes) / sizeof(clientes[0]));
                 listarClientes(clientes, sizeof(clientes) / sizeof(clientes[0]));
+                break;
+            case 6:
+                listarFornecedores(fornecedores, sizeof(fornecedores) / sizeof(fornecedores[0]));
+                break;
+            case 7:
+                ordenarFornecedoresPorNome(fornecedores, sizeof(fornecedores) / sizeof(fornecedores[0]));
+                listarFornecedores(fornecedores, sizeof(fornecedores) / sizeof(fornecedores[0]));
                 break;
             case 0:
                 printf("Encerrando o programa. Até mais!\n");
@@ -129,6 +155,21 @@ void listarClientes(Cliente clientes[], int n) {
         printf("Telefone: %s\n", clientes[i].telefone);
         printf("Endereço: %s\n", clientes[i].endereco);
         printf("UF: %s\n\n", clientes[i].uf);
+    }
+}
+
+/*A função listarFornecedores é responsável por exibir na tela as informações dos
+ fornecedores armazenadas em um array de estruturas do tipo Fornecedor.*/
+void listarFornecedores(Fornecedor fornecedores[], int n) {
+    printf("\nLista de Fornecedores:\n");
+    // Inicia um loop for que percorre todos os fornecedores no array.
+    for (int i = 0; i < n; i++) {
+        printf("Nome: %s\n", fornecedores[i].nome);
+        printf("Endereço Eletrônico: %s\n", fornecedores[i].enderecoEletronico);
+        printf("E-mail: %s\n", fornecedores[i].email);
+        printf("Telefone: %s\n", fornecedores[i].telefone);
+        printf("Endereço: %s\n", fornecedores[i].endereco);
+        printf("UF: %s\n\n", fornecedores[i].uf);
     }
 }
 
@@ -192,6 +233,28 @@ void ordenarClientesPorNome(Cliente clientes[], int n) {
                 clientes[j] = clientes[j + 1];
                 //Move o cliente armazenado em temp para a posição j + 1.
                 clientes[j + 1] = temp;
+            }
+        }
+    }
+}
+
+/*A função ordenarFornecedoresPorNome é responsável por ordenar um array de fornecedores com base no
+campo nome de cada fornecedor. A ordenação é feita usando o algoritmo de ordenação conhecido como 
+Bubble Sort, que compara elementos adjacentes e os troca se estiverem na ordem errada.*/
+void ordenarFornecedoresPorNome(Fornecedor fornecedores[], int n) {
+    // Este loop percorre todo o array de fornecedores.
+    for (int i = 0; i < n - 1; i++) {
+        /*Este loop compara elementos adjacentes e os troca se estiverem fora de ordem. 
+        À medida que o loop externo avança, os maiores elementos "borbulham" para o final do array.*/
+        for (int j = 0; j < n - i - 1; j++) {
+            /*A função strcmp compara duas strings. Se o resultado for maior que zero, significa que
+            a string em fornecedores[j].nome é maior do que a string em fornecedores[j + 1].nome, 
+            indicando que a troca é necessária.*/
+            if (strcmp(fornecedores[j].nome, fornecedores[j + 1].nome) > 0) {
+                //As estruturas Fornecedor em fornecedores[j] e fornecedores[j + 1] são trocadas para reorganizar os elementos do array.
+                Fornecedor temp = fornecedores[j];
+                fornecedores[j] = fornecedores[j + 1];
+                fornecedores[j + 1] = temp;
             }
         }
     }
